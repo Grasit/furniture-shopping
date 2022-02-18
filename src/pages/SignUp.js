4e91ff8aca15c5c0b2e269937bb0bcc5da1d5022
grasit <idongesiteshiet980@gmail.com>
2022-02-19 00:49:09 +0100

    created signup page

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,13 +1,12 @@
 import React, {useState} from "react";
 import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
 import HorizontalLine from "../elements/HorizontalLine";
 
-const LogIn = () => {
+const SignUp = () => {
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
     const onSubmit = data => console.log(data);
    return( 
-        <div className=" grid grid-rows-[1fr 1fr 1fr 1fr 5fr 1fr] gap-4 p-4 relative">
+        <div className=" grid grid-rows-[1fr 1fr 1fr 5fr] gap-4 p-4 relative">
             <div className="flex flex-row justify-between row-start-1 row-span-1">
                 <p className="text-md text-black font-['SF Pro Text'] w-12">
                     9:41
@@ -26,34 +25,35 @@ const LogIn = () => {
                 </div>
                 <HorizontalLine/>
             </div>
-            <div className="text-3xl text-gray-4 font-['Merriweather'] row-start-3 row-span-1">
-                <p>Hello  !</p>
+            <div className="text-2xl my-10 font-semibold uppercase font-['Merriweather'] text-black-font row-start-3 row-span-1">
+                <p>Welcome</p>
             </div>
-            <div className="text-2xl font-semibold uppercase font-['Merriweather'] text-black-font row-start-4 row-span-1">
-                <p>Welcome back</p>
-            </div>
-            <div className="row-start-5 row-span-5 shadow-[0px_7px_30px_rgba(138,149,158,0.2)] absolute left-0 w-11/12">
+            <div className="row-start-4 row-span-7 shadow-[0px_7px_30px_rgba(138,149,158,0.2)] absolute left-0 w-11/12">
                 <form onSubmit={handleSubmit(onSubmit)} className="p-2">
+                    <label className=" text-grey-2"> Name</label>
+                    <input {...register('name', {required : true})} 
+                         className="border-b-2 border-gray-4 w-full p-2 outline-none"/> 
+                         <p>{errors?.name?.type === 'required' && "Name is required"}</p> 
                     <label className=" text-grey-2"> Email</label>
                     <input {...register('email', {required : true})} 
                          className="border-b-2 border-gray-4 w-full p-2 outline-none"/> 
                          <p>{errors?.email?.type === 'required' && "Email is required"}</p> 
                     <label className=" text-grey-2"> Password</label>
-                    <input {...register ("password",{register: true})}  
+                    <input {...register('password', {required : true})} 
+                         className="border-b-2 border-gray-4 w-full p-2 outline-none"/> 
+                         <p>{errors?.password?.type === 'required' && "Password is required"}</p> 
+                    <label className=" text-grey-2"> Confirm Password</label>
+                    <input {...register ("repeatpassword",{register: true})}  
                      className="border-b-2 border-gray-4 w-full p-2 outline-none"/>
-                        <p>{errors?.password?.type === 'required' && 'Password is required'}</p>
-                    <p className="my-10 text-center font-semibold text-lg">Forgot Password</p>
-                    <input type="submit" className=" mx-auto p-2 bg-black-font text-white text-xl w-11/12 rounded-lg outline-none shadow-[0px_10px_20px_rgba(48, 48, 48, 0.25)]" value="Log In"/>
-                    <Link to={"/SignUp"}>
-                        <button className="uppercase mx-auto my-4 text-lg font-semibold flex self-center">Sign Up
-                        </button>
-                    </Link>
+                        <p>{errors?.repeatpassword?.type === 'required' && "Password doesn't match"}</p>
+                    <input type="submit" className=" mx-auto my-10 p-2 bg-black-font text-white text-xl w-11/12 rounded-lg outline-none shadow-[0px_10px_20px_rgba(48, 48, 48, 0.25)]" value="SIGN UP"/>
+                    <p className=" text-center mx-auto text-sm text-['#808080']">Already have an account?
+                         <span className="font-semibold uppercase text-md text-black ml-4">Sign In</span>
+                     </p>
                 </form>
             </div>
-           
-            <div></div>
         </div>
    )
 }
 
-export default LogIn;
+export default SignUp;
