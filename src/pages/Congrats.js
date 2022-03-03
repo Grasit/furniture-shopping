dd5829f4f2893b69c0e712cac512030d41457289
grasit <idongesiteshiet980@gmail.com>
2022-03-03 10:05:31 +0100

    made changes

--- src/pages/Congrats.js
+++ src/pages/Congrats.js
@@ -10,12 +10,12 @@ const Congrats =() => {
             <div className="relative ml-8">
                 <img src="/images/Congrats bg.png" alt="a background" className="relative top-16 w-64 ml-2"/>
                 <img src="/images/Congrats.png" alt="a table set" className="absolute ml-8 top-20"/>
-                <img src="/images/Check Mark.png" alt="a check mark" className="absolute top-[17rem] left-[7rem] w-10 h-10"/>
+                <img src="/images/Check Mark.png" alt="a check mark" className="absolute top-[16rem] left-[7rem] w-10 h-10"/>
             </div>
-            <p className="text-lg text-black3 mt-28 w-[18rem] mx-auto">Your order will be delivered soon.Thank you for choosing our app!
+            <p className="text-lg text-black3 mt-28 w-[19rem] mx-auto whitespace-normal">Your order will be delivered soon.      Thank you for choosing our app!
             </p>
             <Link to={""}>
-                <button className="p-4 text-white bg-black rounded-lg w-full text-xl font-semibold my-4 shadow-[0px_10px_20px_rgba(48,48,48,0.25)]">Track your orders</button>
+                <button className="p-4 text-white bg-black rounded-lg w-full text-xl font-semibold my-6 shadow-[0px_10px_20px_rgba(48,48,48,0.25)]">Track your orders</button>
             </Link>
             <Link to={"/Home"}>
                 <button className="p-4 border border-black text-black bg-white rounded-lg w-full text-lg font-semibold uppercase ">back to home</button>
