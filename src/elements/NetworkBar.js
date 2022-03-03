75ea8963314a5544ba0a86535192cd1aadf34dc1
grasit <idongesiteshiet980@gmail.com>
2022-03-03 11:26:54 +0100

    made changes

--- src/elements/NetworkBar.js
+++ src/elements/NetworkBar.js
@@ -2,16 +2,16 @@ import React from "react";
 
 const NetworkBar = () => {
     return (
-        <div className="flex flex-row justify-between row-start-1 row-span-1 py-2">
-        <p className="text-md text-black font-['SF Pro Text'] w-12">
-            9:41
-        </p>
-        <div className="flex flex-row space-x-2 w-16">
-            <img src="/images/Combined Shape.png" alt="network bar" className="w-4 h-3"/>
-            <img src="/images/Wi-Fi.png" alt=" wi-fi bar" className="w-3 h-3"/>
-            <img src="/images/Battery.png" alt="battery" className="w-4 h-3"/>
+        <div className="flex flex-row justify-between row-start-1 row-span-1 p-4">
+            <p className="text-md text-black font-['SF Pro Text'] w-12">
+                9:41
+            </p>
+            <div className="flex flex-row space-x-2 w-16">
+                <img src="/images/Combined Shape.png" alt="network bar" className="w-4 h-3"/>
+                <img src="/images/Wi-Fi.png" alt=" wi-fi bar" className="w-3 h-3"/>
+                <img src="/images/Battery.png" alt="battery" className="w-4 h-3"/>
+            </div>
         </div>
-    </div>
     )
 }
 
